1f6d36221fb992f71a6ddbc460559eb594c649f7
Douglas AC <dac17@inf.ufpr.br>
2021-02-25 16:32:38 -0300

    Separando funções auxiliares em C em biblioteca 'helper.c'. Definindo makefile para projeto.

--- Trabalho_Final/burrowWheeler.cu
+++ Trabalho_Final/burrowWheeler.cu
@@ -2,75 +2,13 @@
 #include <stdio.h>
 #include <string.h>
 
+extern "C" { 
+    #include "helper.h"
+}
 
 // #define NTHREADS_TOTAL 1024
-#define NTHREADS_TOTAL 32
+#define NTHREADS_TOTAL 4
 #define TAM_ARRAY 100
-
-// TRATA DE ENTRADA DE ARGUMENTOS, IDENTIFICANDO ARQUIVO DE ENTRADA ('in') E SAIDA ('out').
-void identificaArquivosEntrada(FILE **in, FILE **out, int argc, char *argv[]){
-    int i = 1;
-    if(argc > 4){
-        while(i < argc){
-            if(!strcmp(argv[i], "-i")){
-                i++;
-                *in = fopen(argv[i], "r");
-                if(!(*in)){
-                    printf("Erro ao acessar arquivo de entrada '%s'.\n", argv[i]);
-                    exit(1);
-                }
-                i++;
-            }
-            else
-                if(!strcmp(argv[i], "-o")){
-                    i++;
-                    *out = fopen(argv[i], "w");
-                    if(!(*out)){
-                        printf("Erro ao acessar arquivo de saida '%s'.\n", argv[i]);
-                        exit(1);
-                    }
-                    i++;
-                }
-                else{
-                    printf("Arrgumento '%s' inapropriado. Terminando Programa.\n", argv[i]);
-                    exit(1);
-                }
-        }
-    }
-    else{
-        printf("Parametros '-i arq_entrada -o arq_saida' são obrigatórios.\n");
-        exit(1);  
-    }
-}
-
-// RETORNA O TAMANHO, EM BYTES, DO ARQUIVO 'f_in'.
-long int identificaTamanhoEntrada(FILE *f_in){
-
-    long int file_size;
-
-    fseek(f_in, 0L, SEEK_END);
-    file_size = ftell(f_in);
-
-    printf("Arquivo de entrada possui %ld Bytes.\n", file_size);
-
-    rewind(f_in);
-    return file_size;
-}
-
-// RECUPERANDO DADOS DE ARQUIVO DE ENTRADA 'f_in' E ARMAZENANDO-OS EM 'vetor_entrada[]', SENDO 'tamanho' A QUANTIDADE DE BYTES EM 'f_in'.
-void recuperaDadosEntrada(FILE *f_in, char *vetor_entrada[], long int tamanho){
-
-    *vetor_entrada = (char *) malloc(tamanho + 1);
-    if(!(*vetor_entrada)){
-        printf("Erro ao alocar memória para Arquivo de entrada.\n");
-    }
-    
-    for(int i = 0; i < tamanho; i++){
-        (*vetor_entrada)[i] = fgetc(f_in);
-    }
-
-    rewind(f_in);
-}
 
 
 // FUNÇÃO QUE COMPARA DUAS STRINGS, RETORNANDO 0 CASO SEJAM IGUAIS, OU A DIFERENÇA ENTRE OS CARACTERES QUE A DIFEREM (NEGATIVO SE 's1' < 's2', POSITIVO CASO CONTRÁRIO).
@@ -96,6 +34,23 @@ __device__ char strncmpCUDA(char *s1, char *s2, int size){
     // CASO AMBAS STRINGS TENHAM OS "size" CARACTERES IGUAIS, RETORNAR 0;
     return 0;
 }
+
+__global__ void imprimeOrdem(long int *ordemPonteiros, long int tamanho){
+
+    // VERIFICA SE PIXEL "i" NÃO EXTRAPOLA IMAGEM ORIGINAL.
+    for(long int i = 0; i < tamanho; i++){
+        printf("%ld\n", ordemPonteiros[i]);
+    }  
+}
+
+__global__ void inicializaOrdemPonteiros(long int *ordemPonteiros, long int tamanho){
+    long int i = blockDim.x * blockIdx.x + threadIdx.x;
+
+    // VERIFICA SE PIXEL "i" NÃO EXTRAPOLA IMAGEM ORIGINAL.
+    if(i < tamanho){
+        ordemPonteiros[i] = i;
+    }  
+}
 __global__ void printChar(char *saida, char  *entrada, long int tamanho){
   
     char rotacao[TAM_ARRAY];
@@ -116,9 +71,6 @@ __global__ void printChar(char *saida, char  *entrada, long int tamanho){
         printf("%s\n", rotacao);
 
     }  
-    else{
-        printf("-\n");
-    }
 }
 
 int main(int argc, char *argv[]) {
@@ -128,6 +80,7 @@ int main(int argc, char *argv[]) {
     char *hostArquivoSaida;
     char *deviceArquivoEntrada;
     char *deviceArquivoSaida;
+    long int *ordemPonteiros;
     long int in_size;
 
     // TRATAMENTO DE ARGUMENTOS DE ENTRADA.
@@ -144,16 +97,23 @@ int main(int argc, char *argv[]) {
     // ALOCA MEMÓRIA PARA 'deviceArquivoEntrada' e 'deviceArquivoSaida'.
     cudaMalloc((void **)&deviceArquivoEntrada, in_size * sizeof(char));
     cudaMalloc((void **)&deviceArquivoSaida, in_size * sizeof(char));
+    cudaMalloc((void **)&ordemPonteiros, in_size * sizeof(long int));
     
 
     cudaMemcpy(deviceArquivoEntrada, hostArquivoEntrada, in_size * sizeof(char), cudaMemcpyHostToDevice);
 
 
-    // DEFINIDO DIMENSÕES DE GRID E BLOCK LINEARES PARA KERNELS rgb2uintKernelSHM E uint2rgbKernelSHM.
+    // DEFINIDO DIMENSÕES DE GRID E BLOCK LINEARES KERNEL 'inicializaOrdemPonteiros'.
+    dim3 DimGridIniciaOrdem((in_size-1)/NTHREADS_TOTAL + 1, 1, 1);
+    dim3 DimBlockIniciaOrdem(NTHREADS_TOTAL, 1, 1);
+
+    inicializaOrdemPonteiros<<<DimGridIniciaOrdem, DimBlockIniciaOrdem>>>(ordemPonteiros, in_size);
+
+    imprimeOrdem<<<1, 1>>>(ordemPonteiros, in_size);
+
     dim3 DimGrid((in_size-1)/NTHREADS_TOTAL + 1, 1, 1);
     dim3 DimBlock(NTHREADS_TOTAL, 1, 1);
-
-    // EFETUANDO TRANSIÇÃO DE CHAR -> INT
+    // KERNEL DE TESTES
     printChar<<<DimGrid, DimBlock>>>(deviceArquivoSaida, deviceArquivoEntrada, in_size);
 
     
@@ -168,6 +128,7 @@ int main(int argc, char *argv[]) {
 
     cudaFree(deviceArquivoEntrada);
     cudaFree(deviceArquivoSaida);
+    cudaFree(ordemPonteiros);
 
     fclose(f_in);
     fclose(f_out);
